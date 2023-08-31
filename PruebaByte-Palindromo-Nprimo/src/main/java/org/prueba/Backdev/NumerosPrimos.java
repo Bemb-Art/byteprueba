4e11f3b4529f1d3e13edb883e0215c128d6494ad
IngBmora <93457782+IngBmora@users.noreply.github.com>
2023-08-30 18:27:32 -0600

    prueba backdev

---
+++ PruebaByte-Palindromo-Nprimo/src/main/java/org/prueba/Backdev/NumerosPrimos.java
@@ -0,0 +1,32 @@
+package org.prueba.Backdev;
+
+public class NumerosPrimos {
+    public static void numeroPrimos(){
+        int count = 0;
+        int number = 2;
+
+        System.out.print("Los primeros 10 números primos son:");
+
+        while (count < 10) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
